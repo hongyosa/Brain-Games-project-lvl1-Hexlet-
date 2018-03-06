c67425f51c757c073dd00d892f78b7f8c46d1380
hongyosa <phlyinlotus@gmail.com>
2018-03-06 14:32:22 +0300

    step-three

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const introduce = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!\n`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
 };
 export default introduce;
